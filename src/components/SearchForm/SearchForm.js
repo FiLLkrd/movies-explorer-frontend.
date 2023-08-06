36308812d9e89cb78f939f649ef0229cf2117310
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-08-06 13:00:02 +0300

    добавляет API

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useCallback } from "react";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
-export default function SearchForm(){
+export default function SearchForm(props){
+    const { query, shortsToggleSwitch, onInputChange, onToggleChange, onSubmit } = props;
+
+    const submitSearchForm = useCallback(
+    (e) => {
+        e.preventDefault();
+        onsubmit(query);
+    },
+    [onSubmit, query]
+    );
+
     return(
         <>
         <section className="search">
-            <form className="search__form">
-                <input className="search__input" type="text" placeholder="Фильм" required></input>
+            <form className="search__form" noValidate onSubmit={submitSearchForm}>
+                <input 
+                className="search__input"
+                id="search__input" 
+                type="search"
+                name="search" 
+                placeholder="Фильм" 
+                required="required"
+                onChange={onInputChange}
+                value={query || ""}
+                autoFocus></input>
                 <button className="search__btn" type="submit"></button>
             </form>
         </section>
