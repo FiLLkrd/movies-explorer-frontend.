3b82b9c9177cca662349cbb28f881c0fa3a8b49f
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-08-12 11:48:07 +0300

    поправил логирование

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -11,7 +11,7 @@ class MainApi {
     }
 
     async register({ name, email, password }) {
-        const res = await fetch(`${this._baseUrl}/sign-up`, {
+        const res = await fetch(`${this._baseUrl}/signup`, {
             method: "POST",
             headers: this._headers,
             body: JSON.stringify({
